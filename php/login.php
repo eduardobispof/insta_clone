bd4b9128ee629eab9cc6fd6e5d41dfffd5122ebe
eduardobispof <eduardobispof@github.com>
2019-02-13 20:53:03 -0300

    finalização do layout da pagina de login

--- php/login.php
+++ php/login.php
@@ -11,44 +11,51 @@ if (isset($_SESSION['user_name'])) {
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
 	<title>Entrar</title>
 	<style type="text/css">
+
+		body{
+			background-color: #f5f5f5;
+		}
+
 		#login{
 			margin-top: 10%;
-			background-color: gray;
-			width: 30%;
+			background-color: #fafafa;
+			width: 20%;
 			border-radius: 30px;
+			box-shadow: 0px 0px 2px black;
 		}
-		#botao_entrar{
+		.botao{
 			border-radius: 10px;
 		}
-		#botao_cadastro{
-			background-color: blue;
+
+		#link{
+			color: #8493fd;
 		}
 	</style>
 </head>
 <body>
-	<div class="container" id="login" align="center">
+	<div class="container" id="login" >
 		<br>
-		<h1 >Entrar</h1>
+		<h1 align="center" >Entrar</h1>
 		<form>
 		  <div class="form-group">
-		    <label for="inputEmail3" class="col-sm-2 col-form-label">usuário</label>
-		    <div class="col-sm-10">
-		      <input type="email" class="form-control" id="inputEmail3" placeholder="usuário">
-		    </div>
+		    <label for="inputEmail3" class=" col-form-label"><h5>usuário:</h5></label>
+
+		      <input type="email" class="form-control" id="" placeholder="usuário">
+
 		  </div>
 		  <div class="form-group">
-		    <label for="inputPassword3" class="col-sm-2 col-form-label">senha</label>
-		    <div class="col-sm-10">
+		    <label for="inputPassword3" class=" col-form-label"><h5>senha:</h5></label>
+
 		      <input type="password" class="form-control" id="inputPassword3" placeholder="senha">
-		    	<a href="" id="link">esqueceu a senha?</a>
-		    </div>
+		    	<a href="" class="btn-link" id="link">esqueceu a senha?</a>
+
 		  </div>
 
 		  <div class="form-group">
-		    <div class="col-sm-10">
-		      <button type="submit" class="btn btn-primary" id="botao_entrar">Entrar</button>
-		      <a href="#"> <button type="submit" class="btn" id="botao_cadastro">Cadastre-se</button> </a>
-		    </div>
+
+		      <button type="submit" class="btn btn-primary botao" >Entrar</button>
+		      <a href="cadastro.php"> <button type="submit" class="btn btn-outline-primary botao" id="botao_cadastro">Cadastre-se</button> </a>
+
 		  </div>
 		</form>
 		<br>
